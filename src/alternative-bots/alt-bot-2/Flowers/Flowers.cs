6422160f9950c6b0104251267acfed4b22fe9adb
FAQIH MUHAMMAD SYUHADA <faqihmsy@gmail.com>
2025-03-24 11:25:01 +0700

    feat : main bot flowers

---
+++ src/alternative-bots/alt-bot-2/Flowers/Flowers.cs
@@ -0,0 +1,60 @@
+using System.IO;
+using System.Drawing;
+using Robocode.TankRoyale.BotApi;
+using Robocode.TankRoyale.BotApi.Events;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+public class Flowers : Bot {
+    private bool movingForward = true;
+
+    private Flowers(BotInfo botInfo) : base(botInfo) {
+        ;
+    }
+
+    static void Main(string[] args) {
+        var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("Flowers.json");
+        var config = builder.Build();
+        var botInfo = BotInfo.FromConfiguration(config);
+        new Flowers(botInfo).Start();
+    }
+
+    public override void Run() {
+        BodyColor = Color.Purple;
+        TurretColor = Color.Orange;
+        RadarColor = Color.Black;
+        ScanColor = Color.Cyan
+        BulletColor = Color.Blue;
+        GunTurnRate = 50;
+
+        while (IsRunning) {
+            MaxSpeed = 8;
+            if (movingForward) {
+                SetTurnRight(30);
+                Forward(500);
+            } else {
+                SetTurnLeft(30);
+                Back(500);
+            }
+        }
+    }
+
+    public override void OnScannedBot(ScannedBotEvent evt) {
+        Fire(3);
+    }
+
+    public override void OnHitBot(HitBotEvent e) {
+        var bearing = BearingTo(e.X , e.Y);
+        if (bearing > -10 && bearing < 10) {
+            Fire(3);
+        }
+        if (e.IsRammed) {
+            TurnLeft(10);
+            movingForward = !movingForward;
+        }
+    }
+
+    public override void OnHitByBullet(HitByBulletEvent e) {
+        movingForward = !movingForward;
+    }
+}
