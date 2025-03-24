02e77d2ed672cb3cfa1db573bfda79ff10fe3b85
Barru Adi <barru.adi@gmail.com>
2025-03-24 11:14:37 +0700

    fix: side anchor to right or left

--- src/alternative-bots/alt-bot-3/BotPinggiran/BotPinggiran.cs
+++ src/alternative-bots/alt-bot-3/BotPinggiran/BotPinggiran.cs
@@ -10,73 +10,39 @@ public class BotPinggiran : Bot
     int turnDirection = 1;
     double moveAmount;
 
-    static void Main(string[] args)
-    {
+    static void Main(string[] args) {
         new BotBarruPattern().Start();
     }
 
     BotPinggiran() : base(BotInfo.FromFile("BotPinggiran.json")) { }
 
-    public override void Run()
-    {
+    public override void Run() {
         turnDirection = 1;
         moveAmount = Math.Max(ArenaWidth, ArenaHeight);
-        TurnRight(Direction % 90);
+        TurnRight(Direction % 180);
         Forward(moveAmount);
 
         TurnRight(90);
         TurnGunRight(90);
-        while (IsRunning) 
-        {
-            Forward((Math.Min(ArenaWidth, ArenaHeight) * turnDirection) - 20);
+        while (IsRunning) {
+            Forward(ArenaHeight * turnDirection) - 20);
         }
     }
 
 
-	public override void OnScannedBot(ScannedBotEvent e) 
-    {
+	public override void OnScannedBot(ScannedBotEvent e) {
         Fire(3);
 	}
 
-    public override void OnHitBot(HitBotEvent e)
-    {
+    public override void OnHitBot(HitBotEvent e) {
         turnDirection *= -1;
     }
 
-	public override void OnHitByBullet(HitByBulletEvent e) 
-    {
+	public override void OnHitByBullet(HitByBulletEvent e) {
         turnDirection *= -1;
 	}
 	
-	public override void OnHitWall(HitWallEvent e) 
-    {
+	public override void OnHitWall(HitWallEvent e) {
 		turnDirection *= -1;
 	}
-
-    private void MoveTo(double x, double y)
-    {
-        TurnToTarget(x, y);
-        Forward(DirectionTo(x, y));
-    }
-
-    private void TurnToTarget(double x, double y)
-    {
-        var bearing = BearingTo(x, y);
-        if (bearing >= 0)
-            turnDirection = 1;
-        else
-            turnDirection = -1;
-        TurnLeft(bearing);
-    }
-
-    private void TurnOpposite(double x, double y)
-    {
-        var bearing = BearingTo(x, y);
-        if (bearing >= 0)
-            turnDirection = -1;
-        else
-            turnDirection = 1;
-        TurnLeft(bearing + 180);
-    }
-
 }
